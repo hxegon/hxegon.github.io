387f40019ddb38cc5ba57f1f2b0cfaeb3546f030
Cooper LeBrun <cooperlebrun@gmail.com>
2024-04-10 12:39:10 -0400

    remove devShell from flake, I don't use it

--- flake.nix
+++ flake.nix
@@ -8,5 +8,5 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       buildInputs = with pkgs; [ hugo just ];
-    in rec { devShell.${system} = pkgs.mkShell { inherit buildInputs; }; };
+    in { };
 }
